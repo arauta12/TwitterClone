c58c264bfea79669e5120132cb6af40cdcac6f6c
arauta12 <andreirauta234@gmail.com>
2025-01-02 14:47:20 -0500

    Added basics

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConfig');
+const PORT = process.env.PORT || 3500;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', require('./route/root'));
+
+app.all('*', (req, res) => {
+    console.log(`${req.method} request for ${req.url}...`);
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found"})
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+});
